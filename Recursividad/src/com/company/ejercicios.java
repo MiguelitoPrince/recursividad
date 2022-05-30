89203380d868a76d575a324a848482bb32a93b5b
MiguelitoPrince <101578387+MiguelitoPrince@users.noreply.github.com>
2022-05-30 14:58:19 +0200

    Add files via upload

---
+++ Recursividad/src/com/company/ejercicios.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class ejercicios {
+    public static int factorial(int n){
+        int factorial;
+
+        if(n == 1){
+            factorial = n;
+        }else{
+            factorial = n * factorial(n - 1);
+        }
+
+         return factorial;
+
+    }
+}
